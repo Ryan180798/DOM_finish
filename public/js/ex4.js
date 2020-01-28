179977d162e416e8d2a8b464a80b797703e792de
Samira Chahide <samirachahide@MacBook-Pro-de-Samira.local>
2020-01-28 15:43:49 +0100

    ex4

---
+++ public/js/ex4.js
@@ -0,0 +1,23 @@
+var x = document.getElementById('carrer');
+var z = document.getElementById('carrer2');
+var nbclick = 0;
+
+btnj.onclick = function myFonction() {
+  if (x.style.display == 'block') {
+    x.style.display = 'none';
+    z.style.display = 'block';
+    
+  }
+  else{
+    x.style.display = 'block';
+    z.style.display = 'none';
+  } 
+  nbclick += 1;
+  document.getElementById("nbclick").innerHTML = nbclick;
+}
+ 
+
+
+
+
+
