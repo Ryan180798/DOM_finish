0886bd37052ae7ee08d44e1e7a4fa5b9320fbd40
Samira Chahide <samirachahide@MacBook-Pro-de-Samira.local>
2020-01-29 11:24:27 +0100

    ex5

--- public/js/ex5.js
+++ public/js/ex5.js
@@ -2,15 +2,36 @@
   let div = document.getElementById("o");
   var goo = document.getElementById('goo');
   var goo2 = document.getElementById('goo2');
+  var nbclic = 0;
 
-  goo.onclick = function myFonction() {
+  goo.onclick = function myFunction() {
     var img = document.createElement("img");
     img.setAttribute('src', '../img/oeuf.png');
     div.appendChild(img);
     img.style.height = '150px';
     img.style.width = '100px';
+    
+    nbclic += 1;
+    document.getElementById("nbclic").innerHTML = nbclic;
   }
 
-  goo2.onclick = function myFonction2() {
-    div.removeChild(img);
+  goo2.onclick = function myFunction2() {
+    var list = document.getElementById("o");
+    list.removeChild(list.childNodes[0]);
   }
+
+  goo3.onclick = function myFunction3() {
+    var list = document.getElementById("o");
+    list.innerHTML = "";
+  }
+
+btnc.onclick = function myFonctionn() {
+  nbclic = 0;
+  document.getElementById("nbclic").innerHTML = nbclic;
+}
+
+
+
+
+  
+
