02ffdf82f5687a27e3e8e08e4e8a31b92925fc6d
Samira Chahide <samirachahide@MacBook-Pro-de-Samira.local>
2020-01-30 16:17:42 +0100

    ex7_supprimer

--- public/js/ex7.js
+++ public/js/ex7.js
@@ -4,16 +4,25 @@ let list = document.getElementById('list');
 let inlist = document.getElementById('inlist');
   
 
-gol.onclick = function mygol(){
+gol.onclick = function mygot(){
     if (infaire.value != "") {
-    var gol = document.createElement("list");
-    var br = document.createElement("br");
+        var gol = document.createElement("list");
+        var delet = document.createElement('button');
+        delet.setAttribute("style", "margin-left: 20px; width: 40px; height: 15px; border-radius: 3px");
+        var br = document.createElement("br");
 
       gol.innerHTML = infaire.value;
+      delet.innerHTML = " delete ";
       infaire.value = '';
       list.appendChild(gol);
-      list.appendChild(br);
+      gol.appendChild(delet);
+      gol.appendChild(br);
+      delet.addEventListener('click', () =>{
+        gol.remove();
+      })
     }
+    
+    
     else{
       infaire.classList.add('ERROR');
       infaire.placeholder = 'ERROR';
