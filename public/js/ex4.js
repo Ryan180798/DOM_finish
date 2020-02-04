8b9a02509a53a33bca6c9a6249a0a67a35a32731
Samira Chahide <samirachahide@MacBook-Pro-de-Samira.local>
2020-02-04 09:07:27 +0100

    arrangement-ex7

--- public/js/ex4.js
+++ public/js/ex4.js
@@ -1,3 +1,4 @@
+/*
 var x = document.getElementById('carrer');
 var z = document.getElementById('carrer2');
 var nbclick = 0;
@@ -21,7 +22,178 @@ btnv.onclick = function myFonctionn() {
   nbclick = 0;
   document.getElementById("nbclick").innerHTML = nbclick;
 }
+*/
  
+function exo7() {
+
+  let div = document.getElementById('div7')
+  let todo = document.getElementById('todo')
+  let toutes = document.querySelectorAll('#inputs7 input')[0]
+  let completees = document.querySelectorAll('#inputs7 input')[1]
+  let aFaire = document.querySelectorAll('#inputs7 input')[2]
+  let div71;
+  let p;
+  let checkbox;
+  let span;
+  let close;
+  let hr;
+  let tabClose = [];
+  let tabCheck = [];
+
+  let currentText;
+
+
+  function createElem() {
+      div71 = document.createElement('div')
+      p = document.createElement('p')
+
+      transformToInput()
+
+      checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      span = document.createElement('span')
+      close = document.createTextNode("\u00D7");
+      hr = document.createElement('hr')
+      currentText = document.createElement('input')
+      currentText.type = 'text'
+  }
+
+
+  function transformToInput() {
+      console.log('heeellooo');
+      
+      tabCheck.forEach(event => {
+          console.log(event);
+          event.children[1].ondblclick =function () {
+              currentText.value = event.children[1].innerText
+              event.children[1].innerHTML = ''
+              event.children[1].appendChild(currentText)
+              console.log(event.children);
+
+              event.children[1].addEventListener('keypress', ev => {
+
+                  if (ev.key == 'Enter' && currentText.value != '') {
+
+                      p = event.children[1];
+                      p.innerHTML = currentText.value
+                      event.children[1].appendChild(p)
+                      console.log(event.children);
+                  }
+              })
+
+          }
+
+      })
+
+  }
+
+  function addElem() {
+      span.appendChild(close)
+      p.innerText = todo.value
+      div71.appendChild(checkbox)
+      p.appendChild(hr)
+      div71.appendChild(p)
+      tabClose.push(span)
+      div71.appendChild(span)
+      div71.classList.add('div71')
+      tabCheck.push(div71)
+      div.appendChild(div71)
+
+  }
+
+  function deleteElem() {
+
+      tabClose.forEach(elem => {
+          elem.onclick = function () {
+              let div = this.parentElement
+              div.remove()
+          }
+      })
+  }
+
+  function colorCheck() {
+      tabCheck.forEach(elem => {
+
+          //console.log(elem.children[1]);
+
+          if (elem.children[0].checked) {
+              elem.style.backgroundColor = '#8bec3b'
+
+          } else {
+              elem.style.backgroundColor = ''
+
+          }
+
+      })
+  }
+
+
+  todo.addEventListener('focus', () => {
+      document.addEventListener('keypress', event => {
+
+          if (event.key == 'Enter' && todo.value != '') {
+              createElem()
+              addElem()
+              deleteElem()
+              todo.value = ''
+          }
+      })
+  })
+
+
+  div.addEventListener('click', colorCheck)
+
+
+  toutes.addEventListener('click', () => {
+      tabCheck.forEach(element => {
+          element.style.display = 'flex'
+      })
+      toutes.classList.remove('inputs71')
+      toutes.classList.add('selection')
+      completees.classList.add('inputs71')
+      aFaire.classList.add('inputs71')
+
+
+
+  })
+  completees.addEventListener('click', () => {
+      tabCheck.forEach(element => {
+          if (element.children[0].checked) {
+              element.style.display = 'flex'
+          } else {
+              element.style.display = 'none'
+          }
+      })
+      completees.classList.remove('inputs71')
+      completees.classList.add('selection')
+      toutes.classList.add('inputs71')
+      aFaire.classList.add('inputs71')
+  })
+  aFaire.addEventListener('click', () => {
+      tabCheck.forEach(element => {
+          if (element.children[0].checked) {
+              element.style.display = 'none'
+          } else {
+              element.style.display = 'flex'
+          }
+      })
+      aFaire.classList.remove('inputs71')
+      aFaire.classList.add('selection')
+      completees.classList.add('inputs71')
+      toutes.classList.add('inputs71')
+  })
+
+
+
+
+  // div.addEventListener('dblclick',event =>{
+  //     console.log('sndjkvbjksbvj');
+
+  // })
+
+
+
+}
 
 
 
--- public/js/ex7.js
+++ public/js/ex7.js
@@ -1,25 +1,53 @@
+
+let divp = document.getElementById('page7');
+let div = document.getElementById('sect');
 let infaire = document.getElementById('faire');
 let gol = document.getElementById('gol');
 let list = document.getElementById('list');
-let inlist = document.getElementById('inlist');
-  
+
+ 
 
 gol.onclick = function mygot(){
     if (infaire.value != "") {
         var gol = document.createElement("list");
+        var divo = document.createElement("div");
+        var check = document.createElement('input');
+        check.type = "checkbox";
+        check.name = "name"; 
+        check.value = "value"; 
+        check.id = "id"; 
         var delet = document.createElement('button');
-        delet.setAttribute("style", "margin-left: 20px; width: 40px; height: 15px; border-radius: 3px");
+        delet.type = "button";
+        
+
+        delet.setAttribute("style", "position : relative; margin-left: 35%; width: 40px; height: 20px; border-radius: 3px;");
+
+        gol.setAttribute("style", "position : relative; left:48%; width: 40px; height: 15px; top: -23px;");
+
+        check.setAttribute("style", "position : relative; width: 40px; height: 25px;");
+
+        divo.setAttribute("style", "text-align-center; width: 100%; margin:0px; background-color: lightgrey;");
+
+        list.setAttribute("style", "height: 30px; width: 100%; padding-top: 0px;");
+
         var br = document.createElement("br");
 
       gol.innerHTML = infaire.value;
       delet.innerHTML = " delete ";
       infaire.value = '';
+
+      divo.appendChild(delet);
+      gol.appendChild(divo);
+      list.appendChild(divo);
+      divo.appendChild(check);
+
       list.appendChild(gol);
       gol.appendChild(delet);
       gol.appendChild(br);
       delet.addEventListener('click', () =>{
+        divo.remove();
         gol.remove();
-      })
+      });
     }
     
     
